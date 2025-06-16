e9d274ef20a68169a3902956188270350aedeb13
cjosse <cjosse@bdb.be>
2025-06-16 11:07:14 +0200

    favicons

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,24 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Cédric Josse</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <style>{`
           body {
             font-family: "Times New Roman", Times, serif;
